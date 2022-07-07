ebff9e796989655529231b264822dda3fac08b10
AmakawaZKN <108383788+AmakawaZKN@users.noreply.github.com>
2022-07-07 19:53:01 +0700

    Update _antisticker.js

--- plugins/_antisticker.js
+++ plugins/_antisticker.js
@@ -14,7 +14,7 @@ handler.before = function (m, { isAdmin, isBotAdmin }) {
       if (global.opts) {
         if (isAdmin || !isBotAdmin){		  
         }else{
-          m.reply('*Sticker detected*\nYou will in kick now..!!') // ganti text terserah kamu 
+          m.reply('*Sticker detected*\nAnata akan ku kick :)..!!') // ganti text terserah kamu 
           this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
         }return true
       }
